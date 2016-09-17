c257c3ceba404d29cddcb507a4df94d9b2e8040f
Ricky Catron <catron.ricky@gmail.com>
2016-09-17 14:47:23 -0400

    Notes move

--- setup.js
+++ setup.js
@@ -5,7 +5,7 @@
 function  makeNote(x, y, i) {
     var note = new createjs.Shape();
     note.graphics.beginFill(colorArray[i]).drawRect(10, 500, 80, 80);
-    note.x = x;
+    note.x = x*90;
     note.y = y;
     note.alpha = .9;
     return note;
@@ -14,7 +14,7 @@ function  makeNote(x, y, i) {
 function makeKeyButtons() {
     var keyArray = [];
     for (i = 0; i < 8; i++) {
-        var note = makeNote(i*90, 0, i);
+        var note = makeNote(i, 0, i);
         stage.addChild(note);
         keyArray.push(note);
     }
